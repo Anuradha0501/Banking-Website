b541265f0275f31d90a51f47139af671f7d26e60
Anuradha0501 <71958796+Anuradha0501@users.noreply.github.com>
2021-03-13 22:30:06 +0530

    Delete the-sparks-foundation directory

--- the-sparks-foundation/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('localhost','anu','anu0501','malyaraaj-313537e38d');
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
